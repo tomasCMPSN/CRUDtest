862e61f7da08890a9c69a24136f0ecf779c5060b
Tomas <tomascampusano.10@gmail.com>
2021-12-05 19:59:09 -0300

    Index card tweak

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function createCard(product){
     let grid = document.querySelector(`#grid`);
     grid.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4 p-3">
     <div class="card h-100">
-      <img src="${product.url}" class="card-img-top" alt="${product.product}" style="height: 30rem">
+      <img src="${product.url}" class="card-img-top imgCard" alt="${product.product}">
       <div class="card-body">
         <h5 class="card-title">${product.product}</h5>
         <p class="card-text">${product.description}</p>
